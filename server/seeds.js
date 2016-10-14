d1f3af43cf3963faaec0eb41044a79600bbb72cd
wangty <geekvc@qq.com>
2016-10-14 23:20:43 +0800

    write doc

--- server/seeds.js
+++ server/seeds.js
@@ -1,18 +1,18 @@
 if(Videos.find().count()==0){
-Videos.insert({
-title:'动画',
-url:'http://www.bilibili.com/video/douga.html'
-});
+  Videos.insert({
+    title:'动画',
+    url:'http://www.bilibili.com/video/douga.html'
+  });
 
-Videos.insert({
-title:'番剧',
-url:'http://bangumi.bilibili.com/22/'
-});
+  Videos.insert({
+    title:'番剧',
+    url:'http://bangumi.bilibili.com/22/'
+  });
 
-Videos.insert({
-title:'音乐',
-url:'http://www.bilibili.com/video/music.html'
-});
+  Videos.insert({
+    title:'音乐',
+    url:'http://www.bilibili.com/video/music.html'
+  });
 
 }
 
