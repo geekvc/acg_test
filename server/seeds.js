9adf996a8b97ae1a29ace1bdb1e00d6ca7aef5fe
wangty <geekvc@qq.com>
2016-10-14 21:53:37 +0800

    fix urls

--- server/seeds.js
+++ server/seeds.js
@@ -1,17 +1,17 @@
 if(Videos.find().count()==0){
 Videos.insert({
 title:'动画',
-url:'http://www.baidu.com'
+url:'http://www.bilibili.com/video/douga.html'
 });
 
 Videos.insert({
 title:'番剧',
-url:'http://www.baidu.com'
+url:'http://bangumi.bilibili.com/22/'
 });
 
 Videos.insert({
 title:'音乐',
-url:'http://www.baidu.com'
+url:'http://www.bilibili.com/video/music.html'
 });
 
 }
